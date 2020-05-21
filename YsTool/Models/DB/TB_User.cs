053290ccc9caa03f180f94ff983e60b8f338e87f
Y&S <38879082+TK0431@users.noreply.github.com>
2020-05-21 21:26:47 +0900

    20200521

---
+++ YsTool/Models/DB/TB_User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace YsTool.Models.DB
+{
+    /// <summary>
+    /// User Table
+    /// </summary>
+    [Table("tb_user"), Serializable]
+    public class TB_User : TBBase
+    {
+        [Display(Description = "ID", Order = 1), Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string ID { get; set; }
+
+        [Display(Description = "用户名", Order = 2),Required,StringLength(10)]
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string IP { get; set; }
+
+        public string GroupId { get; set; }
+
+        public int Level { get; set; }
+    }
+
+    public class Map_ank_m_spread_json_data : BaseEntityMap<TB_User>
+    {
+        public Map_ank_m_spread_json_data() : base()
+        {
+            // Index(ank_t_bid_plan_information_company_cd_identification_no_per_key)
+            //Property(t => t.company_cd).HasColumnAnnotation("Index", new IndexAttribute("ank_t_bid_plan_information_company_cd_identification_no_per_key", 0) { IsUnique = true });
+            //Property(t => t.identification_no).HasColumnAnnotation("Index", new IndexAttribute("ank_t_bid_plan_information_company_cd_identification_no_per_key", 1) { IsUnique = true });
+            //Property(t => t.period_no).HasColumnAnnotation("Index", new IndexAttribute("ank_t_bid_plan_information_company_cd_identification_no_per_key", 2) { IsUnique = true });
+            //Property(t => t.project_seq).HasColumnAnnotation("Index", new IndexAttribute("ank_t_bid_plan_information_company_cd_identification_no_per_key", 3) { IsUnique = true });
+            //Property(t => t.branch_no).HasColumnAnnotation("Index", new IndexAttribute("ank_t_bid_plan_information_company_cd_identification_no_per_key", 4) { IsUnique = true });
+            //Property(t => t.prcs_kubun_cd).HasColumnAnnotation("Index", new IndexAttribute("ank_t_bid_plan_information_company_cd_identification_no_per_key", 5) { IsUnique = true });
+        }
+    }
+}
---
+++ YsTool/Models/YsDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using YsTool.Models.DB;
+
+namespace YsTool.Models
+{
+    public class YsDbContext: DbContext
+    {
+        public YsDbContext() : base("name=KebDbContext")
+        {
+            Database.SetInitializer<YsDbContext>(null);
+            Database.Log = x =>
+            {
+                //Log4.WriteInfo(x.LastIndexOf("\r\n") > 0 ? x.Remove(x.LastIndexOf("\r\n"), 2) : x);
+            };
+        }
+
+        //public virtual DbSet<ank_m_intermediate_end_reason> ank_m_intermediate_end_reason { get; set; }
+        //public virtual DbSet<ank_m_project_end_bid_result_kubun> ank_m_project_end_bid_result_kubun { get; set; }
+        //public virtual DbSet<ank_m_spread_json_data> ank_m_spread_json_data { get; set; }
+        //public virtual DbSet<to_project_all_recieve_info_recv0205> to_project_all_recieve_info_recv0205 { get; set; }
+        //public virtual DbSet<to_purchase_specification> to_purchase_specification { get; set; }
+        //public virtual DbSet<to_sales> to_sales { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Properties().Where(p => p.Name == "UpdateTime").Configure(p => p.IsConcurrencyToken());
+
+            //modelBuilder.Configurations.Add(new Map_ank_m_spread_json_data());
+            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes().ToList()
+                .Where(type => !string.IsNullOrEmpty(type.Namespace))
+                .Where(type => type.BaseType != null && type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == typeof(BaseEntityMap<>));
+
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Configurations.Add(configurationInstance);
+            }
+        }
+    }
+}
